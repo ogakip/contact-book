145f33811343e572a1ba8e1b9b2e1598054194c4
ogaki <pauloogaki.principal@gmail.com>
2022-09-20 17:12:55 -0300

    feature: add yup validation in create route

--- backend/src/validations/user.ts
+++ backend/src/validations/user.ts
@@ -8,8 +8,9 @@ export const createUserSchema = object().shape({
       .max(158, "Length must be under 158"),
     password: string()
     .required("Password is required on body request")
-    .matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#/$%/^&/*])(?=.{8,})/,
-    "Password must contain at least 8 characters, 1 capital letter, 1 lower case, 1 number and 1 special character"
-    )
-})
+    .matches(/[a-z]/, "Your password must contain at least one lowercase letter")
+      .matches(/[A-Z]/, "Your password must contain at least one capital letter")
+      .matches(/[0-9]/, "Your password must contain at least one number")
+      .matches(/\W/, "Your password must contain at least one special character")
+      .matches(/^(?!.*\s).{0,}$/, "Your password cannot contain spaces")
+}).noUnknown(true)
