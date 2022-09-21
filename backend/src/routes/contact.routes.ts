76227c7cf2a23df0aa71798b4d02fb3254bcbedb
ogaki <pauloogaki.principal@gmail.com>
2022-09-21 15:13:23 -0300

    feature: delete contact controller and service

--- backend/src/routes/contact.routes.ts
+++ backend/src/routes/contact.routes.ts
@@ -9,8 +9,10 @@ import { verifyToken } from "../middlewares/tokenAuth";
 import { schemaValidation } from "../middlewares/schemaValidation";
 
 import { createClientSchema, updateClientSchema } from "../validations/client";
+import { deleteContactController } from "../controllers/contact/deleteContact";
 
 export const ContactRoutes = Router()
 ContactRoutes.post("/:client_id", schemaValidation(createClientSchema), verifyToken, createContactController)
 ContactRoutes.get("/:client_id", verifyToken, readContactsController)
 ContactRoutes.patch("/:contact_id", schemaValidation(updateClientSchema), verifyToken, updateContactController)
+ContactRoutes.delete("/:contact_id", verifyToken, deleteContactController)
