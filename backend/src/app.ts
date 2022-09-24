1160180e95121cf6eeb944b21a81422caea430c0
ogaki <pauloogaki.principal@gmail.com>
2022-09-24 18:34:09 -0300

    feature: add cors to package and app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,8 +5,11 @@ import { ClientRoutes } from "./routes/client.routes"
 import { handleAppErrorMiddleware } from "./middlewares/handleAppError"
 import { ContactRoutes } from "./routes/contact.routes"
 
+const cors = require("cors");
+
 export const app = express()
 app.use(express.json())
+app.use(cors());
 app.use("/user", UserRouter)
 app.use("/clients", ClientRoutes)
 app.use("/contacts", ContactRoutes)
