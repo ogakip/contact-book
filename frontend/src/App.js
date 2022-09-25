1ba2b8a7eb82aa98880c0937dfa39d640fd31873
ogaki <pauloogaki.principal@gmail.com>
2022-09-25 17:11:56 -0300

    feature: add contact

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import { GlobalStyles } from "./styles/global";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'antd/dist/antd.css'
+import "./app.css"
 
 export const App = () => {
   return (
     <div className="app">
       <GlobalStyles/>
       <AppRoutes/>
-      <ToastContainer/>
+      <ToastContainer className="toast"/>
     </div>
   );
 }
